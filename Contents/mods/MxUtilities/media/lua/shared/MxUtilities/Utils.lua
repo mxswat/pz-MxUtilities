ad1429a0b8a1f9326e2f4b96f4977a746f2584a2
mxswat <massimocacciapaglia.13@gmail.com>
2023-11-13 02:00:25 +0000

    new utils!

--- Contents/mods/MxUtilities/media/lua/shared/MxUtilities/Utils.lua
+++ Contents/mods/MxUtilities/media/lua/shared/MxUtilities/Utils.lua
@@ -10,9 +10,35 @@ function Utils:empty(table)
   return table.isempty(table)
 end
 
----@param table table
-function Utils:empty(table)
-  return table.isempty(table)
+---@param target table
+---@param defaults table
+function Utils:mergeWithDefaults(target, defaults)
+  if not target then
+    -- Return a new table to avoid modifying the input
+    return { table.unpack(defaults) }
+  end
+
+  local result = {}
+  for key, defaultValue in pairs(defaults) do
+    result[key] = target[key] ~= nil and target[key] or defaultValue
+  end
+
+  return result
+end
+
+---@param modData table
+---@param modDataKey string
+---@param defaults table
+function Utils:getModDataWithDefault(modData, modDataKey, defaults)
+  local target = modData[modDataKey]
+  if not target then
+    modData[modDataKey] = { table.unpack(defaults) }
+    return modData[modDataKey]
+  end
+
+  -- Always return the modified modData[modDataKey]
+  modData[modDataKey] = self:mergeWithDefaults(target, defaults)
+  return modData[modDataKey]
 end
 
 return Utils
