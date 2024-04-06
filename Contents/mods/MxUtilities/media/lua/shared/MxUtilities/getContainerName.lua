5311a0c59c70cfa6974f4fa2b568ae4ad4bf711d
mxswat <massimocacciapaglia.13@gmail.com>
2024-04-06 20:18:57 +0100

    Refactor getContainerName function to support vehicle containers

--- Contents/mods/MxUtilities/media/lua/shared/MxUtilities/getContainerName.lua
+++ Contents/mods/MxUtilities/media/lua/shared/MxUtilities/getContainerName.lua
@@ -1,11 +1,37 @@
 local getContainerIndex = require "MxUtilities/getContainerIndex"
 local getModDataSafe    = require "MxUtilities/getModDataSafe"
 
+---@param vehicle BaseVehicle
+---@param container ItemContainer
+---@param fallback string
+local function getVehicleContainerName(vehicle, container, fallback)
+  for partIndex = 1, vehicle:getPartCount() do
+    local vehiclePart = vehicle:getPartByIndex(partIndex - 1)
+    if vehiclePart:getItemContainer() == container then
+      return getText("IGUI_VehiclePart" .. vehiclePart:getItemContainer():getType())
+    end
+  end
+
+  return fallback
+end
+
 --- Gets container name from `IGUI_ContainerTitle_[containerType]` or `modData['CustomContainerName']` or empty string
 ---@param container ItemContainer
+---@param fallback? string
 ---@return string
-local function getContainerName(container)
-  local modData = container:getParent():getModData()
+local function getContainerName(container, fallback)
+  fallback = fallback or ""
+
+  local object = container:getParent()
+  if not object then
+    return fallback
+  end
+
+  if instanceof(object, "BaseVehicle") then
+    return getVehicleContainerName(object --[[@as BaseVehicle]], container, fallback)
+  end
+
+  local modData = object:getModData()
   local containerIndex = getContainerIndex(container)
   local customName = getModDataSafe(modData, 'CustomContainerName', {})[containerIndex]
 
